715213cad90f7f15c23742ba05aafec4af25ffd4
H4RD <vszub24@yandex.ru>
2025-02-28 11:13:08 +0300

    Create tests.py

---
+++ myproject/tasks/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from .models import Task
+
+class TaskModelTest(TestCase):
+    def test_create_task(self):
+        task = Task.objects.create(title="Test Task", description="Test Desc", completed=False)
+        self.assertEqual(task.title, "Test Task")
+        self.assertFalse(task.completed)
+
+# Create your tests here.
